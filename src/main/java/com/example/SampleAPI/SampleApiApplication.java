187482730ed34b8095a2745dad47ab85f6fdf1e1
madhu <madhu1121@gmail.com>
2021-12-13 17:24:24 +0530

    gcp related config

--- src/main/java/com/example/SampleAPI/SampleApiApplication.java
+++ src/main/java/com/example/SampleAPI/SampleApiApplication.java
@@ -3,7 +3,10 @@ package com.example.SampleAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
@@ -18,6 +21,13 @@ public class SampleApiApplication {
 	@GetMapping("/welcome")
 	public String hello() {
 		return "Hello madhu";
+	}
+
+	@GetMapping("/welcome/{firstName}/{lastName}")
+	public ResponseEntity<String> getCustomGreeting(@PathVariable String firstName, @PathVariable String lastName){
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.body("hello"+firstName+""+lastName);
 	}
 
 }
